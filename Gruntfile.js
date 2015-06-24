cfc50fabb031d0dcc1be612d39067eb58cea0c16
Paul Rodwell <paul.rodwell@btinternet.com>
2015-06-24 09:18:37 +0100

    sanitize the caption, rather than escape HTML
    plus update build script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-git-authors');
 
   grunt.initConfig({
 
@@ -11,44 +13,30 @@ module.exports = function (grunt) {
       ]
     },
 
-    coffee: {
-      client: {
-        expand: true,
+    browserify: {
+      packageClient: {
+        src: ['client/chart.coffee'],
+        dest: 'client/chart.js',
         options: {
-          sourceMap: true
-        },
-        src: ['client/*.coffee'],
-        ext: '.js'
+          transform: ['coffeeify'],
+          browserifyOptions: {
+            extensions: ".coffee"
+          }
+        }
       }
     },
 
     watch: {
       all: {
         files: ['client/*.coffee'],
-        tasks: ['coffee']
+        tasks: ['browserify']
       }
     }
   });
 
-  grunt.registerTask( "update-authors", function () {
-    var getAuthors = require("grunt-git-authors"),
-    done = this.async();
 
-    getAuthors({
-      priorAuthors: grunt.config( "authors.prior")
-      }, function(error, authors) {
-        if (error) {
-          grunt.log.error(error);
-          return done(false);
-        }
 
-        grunt.file.write("AUTHORS.txt",
-          "Authors ordered by first contribution\n\n" +
-          authors.join("\n") + "\n");
-      });
-  });
-
-  grunt.registerTask('build', ['coffee']);
+  grunt.registerTask('build', ['browserify']);
   grunt.registerTask('default', ['build']);
 
 };
