04eb174ab057bec3b013e4c804dd70e6bc5f6d20
Paul Rodwell <paul.rodwell@btinternet.com>
2024-12-31 11:17:59 +0000

    convert to JavaScript

--- src/client/chart.js
+++ src/client/chart.js
@@ -1,57 +1,66 @@
-###
+/*
  * Federated Wiki : Chart Plugin
  *
  * Licensed under the MIT license.
  * https://github.com/fedwiki/wiki-plugin-chart/blob/master/LICENSE.txt
-###
+ */
 
+const last = array => {
+  return array[array.length - 1]
+}
 
-last = (array) ->
-  array[array.length-1]
+const formatTime = time => {
+  const d = new Date(time > 10000000000 ? time : time * 1000)
+  const mo = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][d.getMonth()]
+  let h = d.getHours()
+  const am = h < 12 ? 'AM' : 'PM'
+  h = h === 0 ? 12 : h > 12 ? h - 12 : h
+  const mi = d.getMinutes() < 10 ? '0' : '' + d.getMinutes()
+  return `${h}:${mi} ${am}<br>${d.getDate()} ${mo} ${d.getFullYear()}`
+}
 
-formatTime = (time) ->
-  d = new Date (if time > 10000000000 then time else time*1000)
-  mo = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][d.getMonth()]
-  h = d.getHours()
-  am = if h < 12 then 'AM' else 'PM'
-  h = if h == 0 then 12 else if h > 12 then h - 12 else h
-  mi = (if d.getMinutes() < 10 then "0" else "") + d.getMinutes()
-  "#{h}:#{mi} #{am}<br>#{d.getDate()} #{mo} #{d.getFullYear()}"
+const display = ($item, data) => {
+  const [time, sample] = data
+  $item.find('p:first').text(sample.toFixed(1))
+  $item.find('p:last').html(formatTime(time))
+}
 
-display = ($item, data) ->
-  [time, sample] = data
-  $item.find('p:first').text sample.toFixed(1)
-  $item.find('p:last').html formatTime(time)
+const findData = (item, thumb) => {
+  return item.data.find(data => data[0] === thumb) ?? null
+}
 
-findData = (item, thumb) ->
-  for data in item.data
-    return data if data[0] is thumb
-  null
-
-window.plugins.chart =
-
-  emit: ($item, item) ->
-    [time, sample] = last(item.data)
-    chartElement = $('<p />').addClass('readout').appendTo($item).text(sample)
-    captionElement = $('<p />').html(wiki.resolveLinks(item.caption)).appendTo($item)
-
-  bind: ($item, item) ->
-
-    lastThumb = null
-
-    $item.find('p:first')
-      .on 'mousemove', (e) ->
-        if typeof e.offsetX == "undefined"
+window.plugins.chart = {
+  emit: ($item, item) => {
+    const [time, sample] = last(item.data)
+    const chartElement = $('<p />').addClass('readout').appendTo($item).text(sample)
+    const captionElement = $('<p />').html(wiki.resolveLinks(item.caption)).appendTo($item)
+  },
+  bind: ($item, item) => {
+    let lastThumb = null
+    $item
+      .find('p:first')
+      .on('mousemove', e => {
+        if (typeof e.offsetX == 'undefined') {
           e.offsetX = e.pageX - $(e.target).offset().left
-        return unless (data = item.data[Math.floor(item.data.length * e.offsetX / e.target.offsetWidth)])?
-        [time, sample] = data
-        return if time == lastThumb || null == (lastThumb = time)
-        display $item, data
-        $item.trigger('thumb', +time)
-      .on 'dblclick', () ->
-        wiki.dialog "JSON for #{item.caption}", $('<pre/>').text(JSON.stringify(item.data, null, 2))
+        }
+        const data = item.data[Math.floor((item.data.length * e.offsetX) / e.target.offsetWidth)]
+        if (!data) return
+        const [time, sample] = data
 
-    $('.main').on 'thumb', (evt, thumb) ->
-      if thumb != lastThumb and (data = findData item, thumb)
+        if (time === lastThumb || time === null) return
+        lastThumb = time
+        display($item, data)
+        $item.trigger('thumb', +time)
+      })
+      .on('dblclick', () => {
+        wiki.dialog('JSON for ${item.caption}', $('<pre/>').text(JSON.stringify(item.data, null, 2)))
+      })
+    $('.main').on('thumb', (evt, thumb) => {
+      const data = findData(item, thumb)
+      if (thumb !== lastThumb && data) {
         lastThumb = thumb
-        display $item, data
+        display($item, data)
+      }
+    })
+  },
+}
